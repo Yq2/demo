df5310d2c1f981bf8cbce20cf75eb7acd662cb6a
yangq13 <yangq13@mingyuanyun.com>
2019-01-23 17:46:43 +0800

    add csp

---
+++ csp/main/main.go
@@ -0,0 +1,32 @@
+package main
+
+func main() {
+	origin ,wait := make(chan int,100),make(chan struct{})
+	Processor(origin,wait)
+	for num := 2 ;num < 100000; num++ {
+		origin <- num
+	}
+	close(origin)
+	<- wait
+}
+
+// 求素数
+func Processor(seq chan int,wait chan struct{}) {
+	go func() {
+		prime,ok := <- seq
+		if !ok {
+			close(wait)
+			return
+		}
+		println("prime:%d",prime)
+		out := make(chan int,100)
+		Processor(out,wait)
+		for num := range seq {
+			if num % prime != 0 {
+				out <- num
+			}
+		}
+		close(out)
+	}()
+}
+
