b0d896e5c84aff0dafdc35809604eb73eb48fce9
yq2 <1225807604@qq.com>
2019-04-20 15:23:25 +0800

    future 模式

---
+++ csp/demo/demo11/demo11.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type query struct {
+	sql    chan string
+	result chan string
+}
+
+func exeQuery(q query) {
+	go func() {
+		sql := <-q.sql
+		// 访问数据库
+
+		// 输出结果通道
+		q.result <- "result from " + sql
+	}()
+}
+
+func main() {
+	// 初始化 Query
+	q := query{make(chan string, 1), make(chan string, 1)}
+
+	// 执行query
+	go exeQuery(q)
+
+	// 发送参数
+	q.sql <- "select * from table"
+
+	// 做其他事情
+	time.Sleep(1 * time.Second)
+
+	// 获取结果
+	fmt.Println("<-q.result = ", <-q.result)
+}
